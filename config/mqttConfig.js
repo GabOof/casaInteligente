34eadfbdd64fae466dc0af3fc9e79aed57fcb78b
GabOof <oliveirafonseca.gab@gmail.com>
2024-12-30 11:11:12 -0300

    Add MQTT configuration file with topics for temperature and heater control

---
+++ config/mqttConfig.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mqttHost: "mqtt://localhost:1883", // Broker MQTT local
+  mqttTopic: "home/temperature", // Tópico para publicação de dados de temperatura
+  heaterTopic: "home/heater", // Tópico para controle do aquecedor
+};
