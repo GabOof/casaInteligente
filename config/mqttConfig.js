5ed9ab276577e579c7034e0aad020076ed8873fb
GabOof <oliveirafonseca.gab@gmail.com>
2025-01-04 17:13:55 -0300

    Add heartbeat topic to MQTT configuration

--- config/mqttConfig.js
+++ config/mqttConfig.js
@@ -1,5 +1,6 @@
 const mqttHost = "mqtt://localhost"; // Para broker local
 const mqttTopic = "home/temperature"; // Tópico de temperatura
 const heaterTopic = "home/heater"; // Tópico do aquecedor
+const heartbeatTopic = "home/heartbeat"; // Tópico do heartbeat
 
-module.exports = { mqttHost, mqttTopic, heaterTopic };
+module.exports = { mqttHost, mqttTopic, heaterTopic, heartbeatTopic };
