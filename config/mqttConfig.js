f9601055a699c94e20fe08bba8e696b1cd2d5b9b
GabOof <oliveirafonseca.gab@gmail.com>
2025-01-04 16:16:11 -0300

    Refactor MQTT configuration to use constants for improved readability

--- config/mqttConfig.js
+++ config/mqttConfig.js
@@ -1,5 +1,5 @@
-module.exports = {
-  mqttHost: "mqtt://localhost:1883", // Broker MQTT local
-  mqttTopic: "home/temperature", // Tópico para publicação de dados de temperatura
-  heaterTopic: "home/heater", // Tópico para controle do aquecedor
-};
+const mqttHost = "mqtt://localhost"; // Para broker local
+const mqttTopic = "home/temperature"; // Tópico de temperatura
+const heaterTopic = "home/heater"; // Tópico do aquecedor
+
+module.exports = { mqttHost, mqttTopic, heaterTopic };
